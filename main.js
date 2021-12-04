a4d8f0eda15cde2a79bb4c69b3c79abac239daa2
gitadmen <94184870+gitadmen@users.noreply.github.com>
2021-12-04 22:01:16 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+let firstNumber = 2;
+let secondNumber = 5;
+
+
+function randomfunction(num1, num2) {
+    let suma = num1 + num2;
+    return suma;
+}
+
+let suma = randomfunction(firstNumber, secondNumber);
+console.log(suma);
+
+
+
+
+/*  Prostsza wersja :)
+
+let firstNumber = 3;
+let secondNumber = 5;
+
+function randomfunction(num1, num2) {
+     return num1 + num2;
+}
+
+console.log(randomfunction(firstNumber, secondNumber));
+
+ */
+
